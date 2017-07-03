42ef8ab32dcc8091f31525a5267381a027817c91
陈瑞庆 <382440703@qq.com>
2017-07-03 09:53:20 +0800

    添加项目文件。

---
+++ WebSite/Areas/Invoice/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite.Areas.Invoice.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Invoice/Home
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Invoice ";
+            ViewBag.Message = "Invoice description page.";
+            return View();
+        }
+    }
+}
